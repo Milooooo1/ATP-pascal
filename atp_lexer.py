81a77d09fe09d7feef259fb10213b1584dc57c04
Milo Beliën <milo.belien@student.hu.nl>
2022-02-15 11:54:36 +0100

    First functionality of the tokenizer

--- atp_lexer.py
+++ atp_lexer.py
@@ -1,4 +1,3 @@
-from tkinter import Variable
 from typing import Iterable, List, Type, Tuple
 from enum import Enum
 import re
@@ -24,43 +23,45 @@ class TokensEnum(Enum):
     VARIABLE           = "^[a-zA-Z]+$"
     INTEGER            = "^[0-9]+$"
 
-class Token:
+class Token(object):
     def __init__(self, token_type: Type[Enum], value: str, position: Tuple[int,int]):
         self.type = token_type
         self.value = value
         self.position = position
 
     def __str__(self) -> str:
-        return f'Token({self.type}, {self.value}) on position: ({self.position})'
+        return f"Token({self.type}, {self.value}) with pos: {self.position}"
 
     def __repr__(self) -> str:
         return self.__str__()
 
-# class Lexer(object):
-#     reserved_words = ["program", "label", "const", "type", "procedure", "function", "var", "begin", "end", "div", "mod", "and", "not", "or", "in"]
-
-    # def __init__(self):
-    #     pass
-
-    def tokenize(input : List[str]) -> List[str]:
-        for line in input:
-            print(line)
-
-    def toToken(input : str, position : Tuple[int, int]) -> Type[Enum]:
-        match input:
-            case "=", ":=":
-                return Token(TokensEnum.EQUALS, "=", position)
-            case "+":
-                return Token(TokensEnum.ADD,    "+", position)
-            case "-":
-                return Token(TokensEnum.SUBS,   "-", position)
-            case "*":
-                return Token(TokensEnum.MULTP,  "*", position)
-            case "/":
-                return Token(TokensEnum.DIVIDE, "/", position)
-            case re.match(TokensEnum.INTEGER, input):
+def toToken(input : str, position : Tuple[int, int]) -> Type[Enum]:
+    match input:
+        case "=" | ":=":
+            return Token(TokensEnum.EQUALS, "=", position)
+        case "+":
+            return Token(TokensEnum.ADD,    "+", position)
+        case "-":
+            return Token(TokensEnum.SUBS,   "-", position)
+        case "*":
+            return Token(TokensEnum.MULTP,  "*", position)
+        case "/":
+            return Token(TokensEnum.DIVIDE, "/", position)
+        case _:
+            if re.match("^[0-9]+$", input):
                 return Token(TokensEnum.INTEGER, input, position)
-            case re.match(TokensEnum.VARIABLE, input):
+            elif re.match("^[a-zA-Z]+$", input):
                 return Token(TokensEnum.VARIABLE, input, position)
-            case _:
-                raise TypeError(f'Illegal character: {input}')
+            else: 
+                raise TypeError(f'Illegal token: {input} on line: {position[0]} token number: {position[1]}')
+
+
+def tokenize(input : List[str], res : List[Token]) -> List[Token]:
+    if not input:
+        return res
+    
+    head, *tail = input
+    for index, key in enumerate(head[1].split(" ")):
+        res.append(toToken(key, (head[0], index)))
+        
+    return tokenize(tail, res)
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
-from atp_lexer import tokenize
+from atp_lexer import *
 import sys, os
 import argparse
 import numpy as np
 
 sys.setrecursionlimit(100000)
-sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
-print(sys.path)
 
-parser = argparse.ArgumentParser(description='Comefrom0x10 interpreter')
-parser.add_argument("-f", "--file", required=True, type=str, help="Filepath to a comefromo0x10 file")
+parser = argparse.ArgumentParser(description='Pascal interpreter')
+parser.add_argument("-f", "--file", required=True, type=str, help="Filepath to a Pascal file")
 
 args = parser.parse_args()
 
 with open(args.file, 'r') as file:
-    lines = [(index+1, line) for index, line in enumerate(file)]
+    lines = [(index+1, line.strip()) for index, line in enumerate(file)]
 
-tokenize(lines)
+tokens = tokenize(lines, [])
+for token in tokens:
+    print(token)
