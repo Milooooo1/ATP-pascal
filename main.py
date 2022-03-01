848ecb050e0056df3a9d34e9b60f0fb36a63d2c5
Milooooo1 <2002milo@gmail.com>
2022-03-01 21:24:08 +0100

    Revamped main

--- main.py
+++ main.py
@@ -3,22 +3,43 @@ from parser import *
 import sys, os
 import argparse
 
-sys.setrecursionlimit(100000)
+def cleanLine(line: str):
+    line.expandtabs(4)
+    line = line.replace("\n", "")
+    leading_spaces = len(line) - len(line.lstrip())
+    if leading_spaces > 0:
+        for x in range(0, leading_spaces, 4):
+            line = "INDENT " + line.lstrip()
+    return line
 
-parser = argparse.ArgumentParser(description='Pascal interpreter')
-parser.add_argument("-f", "--file", required=True, type=str, help="Filepath to a Pascal file")
+def main():
+    sys.setrecursionlimit(100000)
 
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Pascal interpreter')
+    parser.add_argument("-f", "--file", required=True, type=str, help="Filepath to a Pascal file")
 
-with open(args.file, 'r') as file:
-    lines = [(index+1, line.replace("\n", "")) for index, line in enumerate(file)]
+    args = parser.parse_args()
 
-lexedLines = tokenize(lines)
-[print(line) for line in lexedLines]
-# [[print(token) for token in tokens] and print() for tokens in lexedLines]
+    with open(args.file, 'r') as file:
+        lines = [(index+1, cleanLine(line)) for index, line in enumerate(file)]
 
-parser = Parser(lexedLines)
-print(parser.parseLine())
-print(parser.parseLine())
-print(parser.parseLine())
-print(parser.parseLine())
+    lexedLines = tokenize(lines)
+    [print(line) for line in lexedLines]
+    print()
+    # [[print(token) for token in tokens] and print() for tokens in lexedLines]
+
+    parser = Parser(lexedLines)
+    print(parser.parseLine())
+    print(parser.parseLine())
+    print(parser.parseLine())
+    print(parser.parseLine())
+    print(parser.parseLine())
+    print(parser.parseLine())
+    print(parser.parseLine())
+    print(parser.parseLine())
+    print(parser.parseLine())
+    print(parser.parseLine())
+    print(parser.parseLine())
+
+if __name__ == "__main__":
+    main()
