4f973a7fa8f48ac6573d67cf1cc889558f192901
Milooooo1 <2002milo@gmail.com>
2022-03-01 21:26:14 +0100

    Updated example Pascal script

--- example.pas
+++ example.pas
@@ -1,7 +1,17 @@
 100 + 200
 (10 + 20) / 2
 a = 100
-b = (10 + 10) / 2
-a < b
+b = (10 + a) / 2
+c = a < b
+a < (7 + b)
+(2 / 2) < 6
 {comment with whitespace}
 {singlecomment}
+IF (a < 0) THEN
+    a = 2
+    a + 2
+ELSE
+    b + 2
+    b = 2
+{seperator}
+{seperator}
