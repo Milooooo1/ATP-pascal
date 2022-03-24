7dae761d10e4a5fd0be87c4bc068e8a7e61ff440
Milooooo1 <2002milo@gmail.com>
2022-03-24 11:51:26 +0100

    Function calls

--- compiler.py
+++ compiler.py
@@ -3,6 +3,7 @@ from tokens import *
 from ast_classes import *
 from interpreter import Interpreter
 import os
+import difflib
 
 # ==========================================================================================================
 #                                              Compiler Object
@@ -40,7 +41,7 @@ class Compiler(object):
     # compile_Var :: Var -> TextIOWrapper -> str -> str
     def compile_Var(self, node: Var, file: TextIOWrapper, destRegister: str = 'R0') -> str:
         '''Load a var from the stack'''
-        file.write(f"\tLDR {destRegister} [SP, #{self.current_scope[node.value] - 4}]\t\t# {node.value} loaded\n")
+        file.write(f"\tLDR {destRegister} [SP, #{self.current_scope[node.value] - 4}]  \t# {node.value} loaded\n")
         return destRegister
     
     # compile_BinOp :: BinOp -> TextIOWrapper -> None
@@ -61,56 +62,60 @@ class Compiler(object):
         elif isinstance(node.left, Num) and isinstance(node.right, Num):
             tmp = Interpreter(self.tree)
             res = tmp.visit_BinOp(node)
-            file.write(f"\tMOV {destRegister} {res}\n")
+            file.write(f"\tMOV {destRegister} #{res}\n")
         elif not isinstance(node.left, BinOp) and not isinstance(node.right, BinOp):
             file.write(f"\tMOV R1 {self.visit(node.left, file)}\n")
             file.write(f"\tMOV R2 {self.visit(node.right, file)}\n")
             file.write(f"\t{opToAsm[node.op.value]} {destRegister} R1 R2\n")
 
-        # if isinstance(node.left, Var):
-        #     self.compile_Var(node.left, file, "R1")
-        # elif isinstance(node.left, Num):
-        #     file.write(f"\tMOV R1 #{node.left.value}\n") 
-        # elif isinstance(node.left, BinOp):
-        #     self.compile_BinOp(node.left, file) 
-        
-        # if isinstance(node.right, Var):
-        #     self.compile_Var(node.right, file, "R2")
-        # elif isinstance(node.right, Num):
-        #     file.write(f"\tMOV R2 #{node.right.value}\n") 
-        # elif isinstance(node.right, BinOp):
-        #     self.compile_BinOp(node.right, file)
-
-        # match node.op.type:
-        #     case TokensEnum.ADD:
-        #         file.write(f"\tADD R0 R1 R2\n")
-        #     case TokensEnum.SUBS:
-        #         file.write(f"\tSUB R0 R1 R2\n")
-        #     case TokensEnum.MULTP:
-        #         file.write(f"\tMUL R0 R1 R2\n")
-
-        # match node.op.type:
-        #     case TokensEnum.ADD:
-        #         file.write(f"\tADD {destRegister} {self.visit(node.left, file) if not isinstance(node.left, BinOp) else str('R1')} {self.visit(node.right, file) if not isinstance(node.right, BinOp) else str('R0')}\n")
-        #     case TokensEnum.SUBS:
-        #         file.write(f"\tSUB {destRegister} {self.visit(node.left, file) if not isinstance(node.left, BinOp) else str('R1')} {self.visit(node.right, file) if not isinstance(node.right, BinOp) else str('R0')}\n")
-        #     case TokensEnum.MULTP:
-        #         file.write(f"\tMUL {destRegister} {self.visit(node.left, file) if not isinstance(node.left, BinOp) else str('R1')} {self.visit(node.right, file) if not isinstance(node.right, BinOp) else str('R0')}\n")
-            
     # compile_Assign :: Assign -> TextIOWrapper -> None
     def compile_Assign(self, node: Assign, file: TextIOWrapper) -> None:
         '''Store a var'''
-        res = self.visit(node.right, file)
-        if (res is not None):
-            file.write(f"\tMOV R0 {res}\n")
-        file.write(f"\tSTR R0 [SP, #{self.current_scope[node.left.value] - 4}]\t\t# {node.left.value} stored\n")
+        res = ""
+        match type(node.right).__name__:
+            case "BinOp":
+                res = self.compile_BinOp(node.right, file)
+                if (res is not None):
+                    file.write(f"\tMOV R0 {res}\n")
+            case "FuncCall":
+                self.compile_FuncCall(node.right, file)
+        file.write(f"\tSTR R0 [SP, #{self.current_scope[node.left.value] - 4}]  \t# {node.left.value} stored\n")
+
+    # compile_FuncCall :: FuncCall -> TextIOWrapper -> None
+    def compile_FuncCall(self, node: FuncCall, file: TextIOWrapper) -> None:
+        try:
+            # Get the first occurance of the function name
+            func = [i for i in self.tree.funcList if i.funcName == node.funcName][0]  
+            file.write(f"\tbl {func.funcName}\n")
+        except IndexError as e:
+            close_matches = " ".join(difflib.get_close_matches(node.funcName, [func.funcName for func in self.tree.funcList]))
+            if len(close_matches) != 0:
+                raise IndexError(f"Function name: {node.funcName} does not exist. Did you mean any of the following functions: {close_matches}?\n")
+            else:
+                raise IndexError(f"Function name: {node.funcName} does not exist.\n")
 
     # compileFunction :: Func -> TextIOWrapper -> str
     def compile_Func(self, funcNode: Func, file: TextIOWrapper) -> str:
         '''Compile function'''
         file.write(".thumb_func\n" + funcNode.funcName + ":\n")
+        
+        # Update the function scope
+        tmp = self.current_scope
+        self.current_scope = {}
+        funcNode.varDeclDict['result'] = 0
+        [funcNode.varDeclDict.update({arg.value : 0}) for arg in funcNode.argList]
+        [self.current_scope.update({var : ((index + 1) * 4)})\
+                    for index, var, in enumerate(funcNode.varDeclDict.keys())]
+        file.write(f"\t{self.current_scope}\n")
+
+        # Compile function body
+        self.compile_LocalScope(self.current_scope, file)
         [self.visit(node, file) for node in funcNode.funcCodeBlock]
+        file.write(f"\tLDR R0 [SP, #{self.current_scope['result']}]  \t# Store result val in R0\n")
+        self.destruct_LocalScope(self.current_scope, file)
         file.write("\n")
+
+        self.current_scope = tmp
 
     # compile_LocalScope :: Dict -> TextIOWrapper -> None
     def compile_LocalScope(self, scope: Dict, file: TextIOWrapper) -> None:
@@ -126,9 +131,9 @@ class Compiler(object):
         file.write("\t.cpu cortex-m0\n\t.text\n\t.align 4")
         [file.write("\n\t.global " + f.funcName) for f in node.funcList]
         file.write("\n\n")
+        node.varDeclDict['result'] = 0
         [self.current_scope.update({var : ((index + 1) * 4)})\
                     for index, var, in enumerate(node.varDeclDict.keys())]
-        self.current_scope['result'] = 0
         
         # Compile all functions
         [self.visit(funcNode, file) for funcNode in node.funcList]
--- out.asm
+++ out.asm
@@ -8,47 +8,62 @@
 
 .thumb_func
 aMinB:
-	LDR R0 [SP, #0]		# a loaded
+	{'c': 4, 'result': 8, 'a': 12, 'b': 16}
+	SUB SP SP #16
+	LDR R0 [SP, #8]  	# a loaded
 	MOV R1 R0
-	LDR R0 [SP, #4]		# b loaded
+	LDR R0 [SP, #12]  	# b loaded
 	MOV R2 R0
 	SUB R0 R1 R2
-	STR R0 [SP, #8]		# c stored
-	LDR R0 [SP, #8]		# c loaded
-	MOV R0 R0
-	STR R0 [SP, #-4]		# result stored
+	STR R0 [SP, #0]  	# c stored
+	STR R0 [SP, #4]  	# result stored
+	LDR R0 [SP, #8]  	# Store result val in R0
+	ADD SP SP #16
 
 .thumb_func
 aPlusB:
-	LDR R0 [SP, #0]		# a loaded
+	{'result': 4, 'a': 8, 'b': 12}
+	SUB SP SP #12
+	LDR R0 [SP, #4]  	# a loaded
 	MOV R1 R0
-	LDR R0 [SP, #4]		# b loaded
+	LDR R0 [SP, #8]  	# b loaded
 	MOV R2 R0
 	ADD R0 R1 R2
-	STR R0 [SP, #-4]		# result stored
+	STR R0 [SP, #0]  	# result stored
+	LDR R0 [SP, #4]  	# Store result val in R0
+	ADD SP SP #12
 
 .thumb_func
 odd:
+	{'nMinEen': 4, 'result': 8, 'n': 12}
+	SUB SP SP #12
 	IfElse
+	LDR R0 [SP, #8]  	# Store result val in R0
+	ADD SP SP #12
 
 .thumb_func
 even:
+	{'nMinEen': 4, 'result': 8, 'n': 12}
+	SUB SP SP #12
 	IfElse
+	LDR R0 [SP, #8]  	# Store result val in R0
+	ADD SP SP #12
 
 example:
-	SUB SP SP #12
-	MOV R0 #2
-	STR R0 [SP, #4]		# b stored
+	SUB SP SP #16
+	STR R0 [SP, #4]  	# b stored
 	MOV R1 #4
-	LDR R0 [SP, #4]		# b loaded
+	LDR R0 [SP, #4]  	# b loaded
 	MOV R2 R0
 	SUB R3 R1 R2
-	LDR R0 [SP, #4]		# b loaded
+	LDR R0 [SP, #4]  	# b loaded
 	MOV R1 R0
 	MOV R2 #1
 	SUB R4 R1 R2
 	ADD R3 R3 R4
-	MOV R4 16
+	MOV R4 #16
 	ADD R0 R3 R4
-	STR R0 [SP, #0]		# a stored
-	ADD SP SP #12
+	STR R0 [SP, #0]  	# a stored
+	bl aMinB
+	STR R0 [SP, #8]  	# c stored
+	ADD SP SP #16
